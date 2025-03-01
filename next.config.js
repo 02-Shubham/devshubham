e03f483fa0de97a96d64d7998f15159c109da86a
ShubhamSahu <shubhamsahu60582@gmail.com>
2025-03-01 16:45:47 +0530

    error fixing

--- next.config.js
+++ next.config.js
@@ -1,21 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+module.exports = {
   experimental: {
-    appDir: true,
+    appDir: true, // Enables the App Router
   },
   optimizeFonts: false,
   images: {
     domains: ["user-images.githubusercontent.com", "cdn.hashnode.com", "github.com"],
   },
-  fontLoaders: [
-    {
-      loader: "@next/font/google",
-      options: {
-        subsets: ["latin"],
-        weight: ["400", "500", "600", "700", "800"],
-      },
-    },
-  ],
 };
 
-module.exports = nextConfig;
+
